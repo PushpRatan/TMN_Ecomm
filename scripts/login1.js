60a0cb5468dea92c138c11c777076438c4b9e148
Mayank Sharma <91526218+Aimhack3r@users.noreply.github.com>
2022-04-02 19:17:11 +0530

    Create login1.js

---
+++ scripts/login1.js
@@ -0,0 +1,39 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+var maxTry = 3;
+
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    //empty field validation
+    if(username == ""|| password == "")
+    {
+        alert("Please check your Credentials");
+    }
+   
+    
+    //if name and password match
+    if (username == "negative" && password == "naseeb") {
+        window.location = "home.html";
+        return false;
+    }
+    //code for try upto 3 chance then disable username and password field
+    else {
+        maxTry--;
+        if (maxTry == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
