6e898c9d2aec1e56cb8e480f426507c4267a0be2
deep-shikha1701 <dsahu2901@gmail.com>
2022-04-10 18:06:13 +0530

    Added Kids Page

---
+++ scripts/kidsProducts.js
@@ -0,0 +1,441 @@
+
+products = [
+    {
+        brand_name: "Puma",
+        img:"https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/5500201/2018/4/24/11524569424344-na-5221524569424114-1.jpg",
+        product_name: "Kids Black Kent V JR Sneakers",
+        price: 1889,
+        discount: 30,
+        oldprice: 2699,
+    },
+    {
+        brand_name: "Hopscotch",
+        img:"https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16334330/2021/12/2/5eb31306-337b-4db9-9089-ef45e0ed1f751638417222084ClothingSet1.jpg",
+        product_name: " Hopscotch Boys Blue & Red Shirt with Pyjamas & Blazer",
+        price: 680,
+        discount: 54,
+        oldprice: 1479,
+
+    },
+
+    {
+        brand_name: "HELLCAT",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17137550/2022/2/11/b124b0f4-830d-44ed-a669-e8585d66cf4f1644579708683HellcatBoysRoundNeckBlendedCottonTshirt-ComboPackof51.jpg",
+        product_name: "Boys Pack Of 5 Typography Printed Bio Finish T-shirt",
+        price: 949,
+        discount: 81,
+        oldprice: 4995,
+
+    },
+    {
+        brand_name: "H&M",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15108378/2022/1/11/e1fdcca3-1e1e-4126-99a0-dbfa009f9f001641885001181-PLUM-TREE-Boys-Grey-Printed-Regular-Shorts-6041641885001099-2.jpg",
+        product_name: "Boys Grey Printed Regular Shorts",
+        price: 499,
+        discount: 50,
+        oldprice: 999,
+
+    },
+    {
+        brand_name: "WatchOut Wearables",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17399030/2022/3/4/17dc906d-0983-4f32-b5ed-2daa2becadf21646397778437SmartWatches1.jpg",
+        product_name: "Kids Black Solid 4G Video Call, Music, Games & Anti-Theft Smart Watches",
+        price: 119999,
+        discount: 30,
+        oldprice: 14999,
+
+    },
+    {
+        brand_name: "Stylo Bug",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14955462/2021/7/27/808b3090-603b-4d32-b550-21e3683e959d1627399841405StyloBugGirlsPinkTieredKurtiwithShararaWithDupatta7.jpg",
+        product_name: "Slim rapid dry Raglan T-shirt",
+        price: 1398,
+        discount: 65,
+        oldprice: 3995,
+
+    },
+    {
+        brand_name: "Urbano Juniors",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11851554/2020/5/14/86794fba-16e9-4962-9cdb-226202f6ae281589444576626-Urbano-Juniors-Boys-Black-Slim-Fit-Jeans-2591589444576051-1.jpg",
+        product_name: "Striped polo collar T-shirt",
+        price: 571,
+        discount: 48,
+        oldprice: 1099,
+
+    },
+    {
+        brand_name: "Nike",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/6676849/2021/12/16/a7082a4a-7d63-4af8-9c03-589f92e38b1f1639636204088-Nike-Boys-Black-Revolution-4-Running-Shoes-6401639636203850-1.jpg",
+        product_name: "Kids Black Revolution 4 Running Shoes",
+        price: 1299,
+        discount: 60,
+        oldprice: 3495,
+
+    },
+    {
+        brand_name: "H&M",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17611506/2022/3/22/84caf339-a63c-4f49-a881-7e9d9b8e980d1647962686047Patternedjerseydress1.jpg",
+        product_name: "Girls Green Patterned Jersey Dress",
+        price: 399,
+        discount: 0,
+        oldprice: 399,
+
+    },
+    {
+        brand_name: "Bella Moda",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14980250/2021/8/10/9570f46f-37a5-4c3f-b3c4-cecdebf05d0f1628595043254-Bella-Moda-Maroon--Black-Dress-2381628595042809-1.jpg",
+        product_name: "Maroon & Black Sequinned Net Fit & Flare Dress with Sling Bag",
+        price: 603,
+        discount: 72,
+        oldprice: 2157,
+
+    },
+    {
+        brand_name: "Nike",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7487529/2021/12/16/db252a58-d434-4e24-a4eb-3fed88ffa13c1639636223137-Nike-Boys-Black-NIKE-REVOLUTION-4-Running-Shoes-124163963622-1.jpg",
+        product_name: "Kids Black NIKE REVOLUTION 4 Running Shoes",
+        price: 629,
+        discount: 10,
+        oldprice: 699,
+    },
+    {
+        brand_name: "BonOrganik",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15334348/2021/9/1/d784aa9b-15a9-41e0-86d1-a9cba85591e71630509125052BonOrganikBoysWhitePrintedV-NeckAppliqueT-shirt1.jpg",
+        product_name: "Boys White Graphic Printed Round-Neck T-shirt",
+        price:299,
+        discount: 40,
+        oldprice: 499,
+
+    },
+    {
+        brand_name: "Cutiekins",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/5/6/bbfea62a-3f1e-4f9d-8e4e-f0aa690d909f1620286359782-1.jpg",
+        product_name: "Girls Pink & White Printed Fit & Flare Dress",
+        price: 559,
+        discount: 72,
+        oldprice: 1999,
+
+    },
+    {
+        brand_name: "pspeaches",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14988650/2021/7/31/f6fe7ffb-af5f-483d-8caf-6b04001566981627705396100pspeachesGirlsPinkStripedPureCottonKurtiwithPatiala1.jpg",
+        product_name: "Girls Pink Floral Striped Pure Cotton Kurti with Patiala",
+        price: 927,
+        discount: 68,
+        oldprice: 9227,
+
+    },
+    {
+        brand_name: "H&M",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16849332/2022/1/21/5f3ac737-ecd8-48a0-9d89-0e98420a327e1642751077877CottonT-shirt1.jpg",
+        product_name: "Boys Green Cotton T-shirt",
+        price: 399,
+        discount: 0,
+        oldprice: 399,
+
+    },
+    {
+        brand_name: "U.S. Polo Assn. Kids",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14194948/2021/7/8/239e535e-4ee0-4bdb-b80a-7197d9cb46ee1625748641115-U-S-Polo-Assn-Kids-Boys-Blue-Washed-Mid-Rise-Denim-Shorts-39-1.jpg",
+        product_name: "Boys Blue Washed Mid-Rise Denim Shorts",
+        price: 2399,
+        discount: 20,
+        oldprice: 2999,
+
+    },
+    {
+        brand_name: "pspeaches",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16901574/2022/1/21/90319996-37f5-43dc-b38f-b0830e18f3131642776751037pspeachesMulticolouredFloralDress1.jpg",
+        product_name: "Multicoloured Floral Dress",
+        price: 2999,
+        discount: 25,
+        oldprice: 3999,
+
+    },
+    {
+        brand_name: "LilPicks",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/1/14/3c217a53-3aa3-4fef-936a-84d465359f141610616879045-1.jpg",
+        product_name: "Girls Pink & White Panda Printed Night Suit",
+        price: 1599,
+        discount: 50,
+        oldprice: 3199,
+
+    },
+    {
+        brand_name: "Red Tape",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15868730/2021/11/3/84054fb4-44e3-48d6-bb01-f80dcfb59ee01635948045683RedTapeUnisexKidsBlueMeshWalkingNon-MarkingShoes1.jpg",
+        product_name: "Unisex Kids Blue Mesh Walking Shoes",
+        price: 699,
+        discount: 50,
+        oldprice: 1399,
+    },
+    {
+        brand_name: "HELLCAT",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17137558/2022/2/11/7c7f4ce3-5a02-4898-bd04-6d39e5e1c5251644579810870HellcatBoysRoundNeckBlendedCottonTshirt-ComboPackof51.jpg",
+        product_name: "Boys Set of 5 Printed T-shirt",
+        price: 449,
+        discount: 50,
+        oldprice: 899,
+
+    },
+    {
+        brand_name: "YK Disney",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17387606/2022/3/4/474955d4-f014-4cf6-aa96-642d30b1c7a91646409743474YKDisneyBoysBlue3PrintedAppliqueT-shirt1.jpg",
+        product_name: "Boys Blue 3 Printed Applique T-shirt",
+        price: 1374,
+        discount: 25,
+        oldprice: 1899,
+
+    },
+    {
+        brand_name: "Cutiekins",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17288464/2022/2/25/8b03e839-a098-4097-80ea-3b344106f4d91645776120683CutiekinsBlackFloralOff-ShoulderCrepeMidiDress1.jpg",
+        product_name: "Black Floral Off-Shoulder High Low Crepe Dress",
+        price: 1299,
+        discount: 25,
+        oldprice: 1799,
+
+    },
+    {
+        brand_name: "PLUM TREE",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15108386/2022/1/11/6880066b-71ac-4455-8be7-db933b491c9e1641885032392-PLUM-TREE-Boys-Red-Printed-Regular-Shorts-8191641885032313-2.jpg",
+        product_name: "Boys Red Printed Regular Shorts",
+        price: 899,
+        discount: 25,
+        oldprice: 1199,
+
+    },
+    {
+        brand_name: "H&M",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17674644/2022/3/29/8b7f4284-5abb-4788-bb7b-1e7de8c0b0581648558377303PrintedT-shirt1.jpg",
+        product_name: "Boys Peach-Coloured Pure Cotton Printed T-Shirt",
+        price: 899,
+        discount: 25,
+        oldprice: 1199,
+
+    },
+    {
+        brand_name: "CUTECUMBER",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16946554/2022/1/27/a0751ad3-e0b5-4e01-b779-006011c86fe31643271355075CUTECUMBERMauveFloralGeorgetteDress1.jpg",
+        product_name: "Pink & White Georgette Dress",
+        price: 899,
+        discount: 25,
+        oldprice: 1199,
+
+    },
+    {
+        brand_name: "Cutiekins",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/10/17/090f9dc1-781d-43ed-a77b-80eb0675a50f1602890526587-1.jpg",
+        product_name: "Girls Peach-Coloured & Green Printed Top with Palazzos",
+        price: 899,
+        discount: 25,
+        oldprice: 1199,
+
+    },
+    {
+        brand_name: "H&M",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17464682/2022/3/10/68078e07-c470-4d66-baf2-446561b6f48a1646917124976Patternedjerseydress1.jpg",
+        product_name: "Girls Beige & Pink Patterned Pure Cotton Jersey Dress",
+        price: 899,
+        discount: 25,
+        oldprice: 1199,
+
+    },
+    {
+        brand_name: "BONKIDS",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/6/12/040bf217-063a-4dc8-96ab-335bef80789f1623519244856-1.jpg",
+        product_name: "X DISNEY T-shirt",
+        price: 899,
+        discount: 50,
+        oldprice: 1799,
+
+    },
+    {
+        brand_name: "Red Tape",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15928978/2021/11/3/ab3e775a-7f4f-46a0-a7c4-81b74163516a1635936884234RedTapeKidsPinkMeshRegularWalkingShoes1.jpg",
+        product_name: "Kids Pink Mesh Regular Walking Shoes",
+        price: 1049,
+        discount: 50,
+        oldprice: 2099,
+
+    },
+    {
+        brand_name: "pspeaches",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17630072/2022/3/24/251f1065-2bce-49d6-bd12-aff15cafc7d71648122946831pspeachesMulticolouredFloralOff-ShoulderMidiDress1.jpg",
+        product_name: "Multicoloured Floral Off-Shoulder Midi Dress",
+        price: 1719,
+        discount: 10,
+        oldprice: 1899,
+
+    },
+    {
+        brand_name: "Pantaloons Baby",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17302106/2022/2/26/e39ee8e0-7925-46d5-8cbb-efcfbcc999bb1645848460482PantaloonsBabyBoysBlueClothingSet1.jpg",
+        product_name: "Self Design round neck T-shirt",
+        price: 849,
+        discount: 50,
+        oldprice: 1999,
+    },
+    {
+        brand_name: "Aj DEZines",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/1/6/67da079f-ed42-4cfc-b4d3-7d99a4e679771609921410998-1.jpg",
+        product_name: "Boys Solid Kurta Set",
+        price: 1599,
+        discount: 20,
+        oldprice: 1999,
+    },
+    {
+        brand_name: "BIBA",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16959880/2022/1/28/7ab29dc4-6d3f-49cd-a493-baf61697d7911643355372723KurtaSets1.jpg",
+        product_name: "Printed kurta with Plazzos",
+        price: 2499,
+        discount: 0,
+        oldprice: 2499,
+    },
+    {
+        brand_name: "CUTECUMBER",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11427716/2022/1/7/05ce3387-733e-4bd7-a60b-7b7d4b59a49d1641551612595-CUTECUMBER-Girls-Red--White-Printed-Fit-and-Flare-Dress-4811-2.jpg",
+        product_name: "Girls Printed fit and Flare dress",
+        price: 2499,
+        discount: 0,
+        oldprice: 2499,
+    },
+    {
+        brand_name: "Kidling",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/6/7/8e36bee5-beee-4572-ae92-3ea2b22666f31623081310707-1.jpg",
+        product_name: "Printed Top with shorts",
+        price: 2499,
+        discount: 0,
+        oldprice: 2499,
+    },
+    {
+        brand_name: "Stylo Bug",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15396092/2021/9/7/415faca5-7b3c-41c5-8e95-11c4f411f8f81631020548440StyloBugGirlsBlueFloralEmbroideredTieredKurtiwithShararaWith1.jpg",
+        product_name: "Girls Blue Floral Embroidered Tiered Kurti with Sharara",
+        price: 2499,
+        discount: 0,
+        oldprice: 2499,
+    },
+
+]
+
+
+
+if (JSON.parse(localStorage.getItem("products")) == null) {
+    localStorage.setItem("products", JSON.stringify(products))
+}
+
+function showProducts(d) {
+    let products = d;
+
+    products.forEach(function (el) {
+        appendProduct(el);
+    })
+}
+
+function appendProduct(el) {
+    let catalogue = document.getElementById('products-right');
+
+    let div = document.createElement("div")
+
+    // console.log(el);
+
+    div.addEventListener("click", function () {
+        localStorage.setItem("clickedProduct", JSON.stringify(el))
+    })
+
+    div.innerHTML = `<a class="each-product" href="./product.html">
+              ><div>
+                <img
+                  src= ${el.img}
+                  alt=""
+                />
+                <div class="brand">${el.brand_name}</div>
+                <div class="name">${el.product_name}</div>
+                <div class="price">
+                 Rs. ${Math.ceil(el.oldprice * (100 - el.discount) / 100)} <span class="line-through">Rs. ${el.oldprice}</span>
+                  <span class="discount">(${el.discount}% OFF)</span>
+                </div>
+              </div></a
+            >`
+    // console.log(div)
+
+
+
+    catalogue.append(div)
+}
+
+showProducts(JSON.parse(localStorage.getItem("products")))
+
+
+
+function sort() {
+    let menu = document.getElementById("type");
+    menu.addEventListener("change", generateData);
+
+    function generateData(event) {
+        if (menu.value == '1') {
+            let products = JSON.parse(localStorage.getItem("products"))
+            let catalogue = document.getElementById('products-right');
+            catalogue.innerHTML = null;
+
+            products = products.sort(function (a, b) {
+                return b.price - a.price
+            });
+
+            showProducts(products);
+        } else if (menu.value == '2') {
+            let products = JSON.parse(localStorage.getItem("products"))
+            let catalogue = document.getElementById('products-right');
+            catalogue.innerHTML = null;
+
+            products = products.sort(function (a, b) {
+                return a.price - b.price
+            });
+
+            showProducts(products);
+        } else if (menu.value == '3') {
+            let products = JSON.parse(localStorage.getItem("products"))
+            let catalogue = document.getElementById('products-right');
+            catalogue.innerHTML = null;
+
+            products = products.sort(function (a, b) {
+                return b.discount - a.discount
+            });
+
+            showProducts(products);
+        }
+        else if (menu.value == '4') {
+            let products = JSON.parse(localStorage.getItem("products"))
+            let catalogue = document.getElementById('products-right');
+            catalogue.innerHTML = null;
+
+            showProducts(products);
+        }
+    }
+}
+sort();
+
+function filter(x) {
+    let products = JSON.parse(localStorage.getItem("products"))
+
+    products = products.filter(function (el) {
+        return el.brand_name == x;
+    })
+
+    let catalogue = document.getElementById('products-right');
+    catalogue.innerHTML = null;
+    showProducts(products);
+}
+
+function priceFilter(x, y) {
+    let products = JSON.parse(localStorage.getItem("products"))
+
+    products = products.filter(function (el) {
+        return el.price < x && el.price > y;
+    })
+
+    let catalogue = document.getElementById('products-right');
+    catalogue.innerHTML = null;
+    showProducts(products);
+}
